15d2930306d6782f123956ae2cef10fd984e69cf
3400 Records <cbowman@3400records.com>
2026-07-09 22:47:12 -0400

    added clean header

--- scripts/main.js
+++ scripts/main.js
@@ -34,15 +34,51 @@ function renderBeats() {
     if(!beatsBox){return;}
     beatsBox.innerHTML = `
         <span class="beats-span">Beats For Sale</span>
-        <div class="beats-button" onclick="updateBeats();">Reveal Beats</div>
     `;
-    for(const [key, value] of Object.entries(loadedBeats)){
-        if("bsid" in value){ // BeatStars Embedding Method
-            const embedFrame = document.createElement("iframe");
-            embedFrame.setAttribute("class", "beats-card");
-            embedFrame.src = `//www.beatstars.com/embed/track/?id=${value["bsid"]}`;
-            beatsBox.appendChild(embedFrame);
+    for(const [key, beatJSON] of Object.entries(loadedBeats)){
+        // if("bsid" in value){ // BeatStars Embedding Method
+        //     const embedFrame = document.createElement("iframe");
+        //     embedFrame.setAttribute("class", "beats-card");
+        //     embedFrame.src = `//www.beatstars.com/embed/track/?id=${value["bsid"]}`;
+        //     beatsBox.appendChild(embedFrame);
+        // }
+
+        const beatsCard = document.createElement("div");
+        beatsCard.setAttribute("class", "beats-card");
+
+        if("title" in beatJSON){
+            const beatsCardTitle = document.createElement("span");
+            beatsCardTitle.setAttribute("class", "beats-card-title");
+            beatsCardTitle.textContent = beatJSON["title"];
+
+            beatsCard.appendChild(beatsCardTitle);
         }
+
+        if("mediasrc" in beatJSON){
+            if("tagged_mp3" in beatJSON["mediasrc"]){
+                const beatsCardAudio = document.createElement("audio");
+                beatsCardAudio.setAttribute("class", "beats-card-audio");
+                beatsCardAudio.textContent = beatJSON["title"];
+
+                const beatsCardAudioSrc = document.createElement("source");
+                beatsCardAudioSrc.setAttribute("type", "audio/mpeg");
+                beatsCardAudioSrc.setAttribute("src", beatJSON["mediasrc"]["tagged_mp3"]);
+                
+                beatsCardAudio.appendChild(beatsCardAudioSrc);
+                beatsCard.appendChild(beatsCardAudio);
+            }
+
+            if("thumbnail" in beatJSON["mediasrc"]){
+                const beatsCardThumbnail = document.createElement("img");
+                beatsCardThumbnail.setAttribute("class", "beats-card-thumbnail");
+                beatsCardThumbnail.setAttribute("src", beatJSON["mediasrc"]["thumbnail"]);
+
+                beatsCard.appendChild(beatsCardThumbnail);
+            }
+
+        }
+
+        beatsBox.appendChild(beatsCard);
     }
 }
 
@@ -51,3 +87,5 @@ async function updateBeats(){
     renderBeats();
 }
 
+// Update the beats on page load
+// updateBeats();
